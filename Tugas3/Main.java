b71d29410a2c5c2cd1dc0fe2c611729c4144ea3d
OktarinaPutri <oktarina.putri@students.untidar.ac.id>
2026-09-13 23:34:00 +0700

    Add files via upload

---
+++ Tugas3/Main.java
@@ -0,0 +1,32 @@
+
+package Tugas3;
+
+/**
+ *
+ * @author USer
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        // 1. Membuat objek Pekerja
+        System.out.println("INFORMASI PEKERJA");
+        Pekerja pekerja1 = new Pekerja("Buminian", 25, "Hr",8000000);
+        System.out.println(pekerja1.toString());
+        
+        // 2. Ubah nama menggunakan setter
+        System.out.println("\n=== SETELAH NAMA DIUBAH ===");
+        pekerja1.setNama("Buminiora");
+        System.out.println(pekerja1.toString());
+        
+        // 3. Uji akses langsung dengan atribut
+        System.out.println("\nUJI AKSES LANGSUNG");
+        
+        // Akses public (pekerjaan) seharusnya bisa
+        System.out.println("Pekerjaan (public): " + pekerja1.pekerjaan);
+        
+        // Akses protected (usia) seharusnya bisa karena package sama
+        System.out.println("Usia (protected)  : " + pekerja1.usia + "tahun");
+        System.out.println("Nama (via getter) : " + pekerja1.getNama());
+        System.out.println("Gaji (via getter) : " + pekerja1.getGaji());
+    }
+}
---
+++ Tugas3/Pekerja.java
@@ -0,0 +1,28 @@
+
+package Tugas3;
+
+/**
+ *
+ * @author USer
+ */
+public class Pekerja extends Manusia {
+    private double gaji;
+    public Pekerja(String nama, int usia, String pekerjaan, int gaji){
+        super(nama, usia, pekerjaan);
+        this.gaji= gaji;
+    }
+    public double getGaji(){
+        return gaji;
+    }
+    public void setGaji(){
+        this.gaji = gaji;
+    }
+    @Override
+    public String toString(){
+        return "Nama     : " + getNama() +"\n" +
+               "Usia     : " + usia + "tahun\n" +
+               "Pekerjaan: " + pekerjaan + "\n" +
+               "Gaji     : Rp" + gaji;
+        
+    }
+}
