22b56b6148894db33e3b640451808a73ce3698f8
OktarinaPutri <oktarina.putri@students.untidar.ac.id>
2026-09-13 19:13:32 +0700

    Add files via upload

---
+++ Proyek1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proyek1;
+
+/**
+ *
+ * @author USer
+ */
+public class Proyek1 {
+    public static void main(String[] args) {
+        String nama   = "Oktarina Putri";
+        String npm    = "2505060002";
+        String alamat = "Sanggrahan";
+        String noHp   = "089505261347";
+
+        System.out.println("• Nama   : " + nama);
+        System.out.println("• NPM    : " + npm);
+        System.out.println("• Alamat : " + alamat);
+        System.out.println("• No_HP  : " + noHp);
+    }
+}
+
+    
+
